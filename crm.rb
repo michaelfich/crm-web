3c36e00959c3c7cc724ebff1854d4363bc8ee299
Michael Fich <michaeltf@gmail.com>
2015-06-16 15:15:46 -0400

    Moved contacts.erb to the views folder to correct an error.

--- crm.rb
+++ crm.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
 
+
 get "/" do
   @crm_app_name = "My CRM"
   erb :index
 end
 
 get "/contacts" do
+  @crm_app_name = "My CRM"
   erb :contacts
+end
+
+get "/contact/new" do
+  p "Add New Contact"
 end
