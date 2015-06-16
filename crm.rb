69042b653f1744c0940186f3a79c52b44620a222
Michael Fich <michaeltf@gmail.com>
2015-06-16 15:25:03 -0400

    Added the contact.rb file and added some temporary contacts in the /contacts route.

--- crm.rb
+++ crm.rb
@@ -1,5 +1,5 @@
+require_relative 'contact'
 require 'sinatra'
-
 
 get "/" do
   @crm_app_name = "My CRM"
@@ -8,6 +8,12 @@ end
 
 get "/contacts" do
   @crm_app_name = "My CRM"
+
+  @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
   erb :contacts
 end
 
