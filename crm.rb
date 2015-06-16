07674515ccb343de28510505032189ed20a6d44b
Michael Fich <michaeltf@gmail.com>
2015-06-16 16:08:54 -0400

    Created new_contact.rb to accept input for a new contact to be added.  Corrected an issue with contacts being added multiple times.

--- crm.rb
+++ crm.rb
@@ -3,6 +3,9 @@ require_relative 'contact'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer"))
+$rolodex.add_contact(Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO"))
+$rolodex.add_contact(Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder"))
 
 get "/" do
   @crm_app_name = "My CRM"
@@ -12,13 +15,10 @@ end
 get "/contacts" do
   @crm_app_name = "My CRM"
 
-  $rolodex.add_contact(Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer"))
-  $rolodex.add_contact(Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO"))
-  $rolodex.add_contact(Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder"))
 
   erb :contacts
 end
 
-get "/contact/new" do
-  p "Add New Contact"
+get "/contacts/new" do
+  erb :new_contact
 end
